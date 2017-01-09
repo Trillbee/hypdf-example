f5bcd37d6890b82c743be5659385fcbd7536a5c0
Trillbee <adelaide@MacBook-Air.local>
2017-01-09 12:52:30 +1100

    try adding in code from Heroku connect demo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   #from heroku connect demo
-  before_filter :authenticate_rails_user!
+  #before_filter :authenticate_rails_user!
 
 end
