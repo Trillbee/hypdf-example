8a77f5df1a4abff1415d35a9c7d4f7967d086793
Trillbee <adelaide@MacBook-Air.local>
2017-01-09 12:55:47 +1100

    try adding in code from Heroku connect demo

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -151,28 +151,28 @@ class PdfController < ApplicationController
 
   #from heroku connect demo
 
-  def accounts
-    @page = (params[:page] || 1).to_i
-    @accounts = Account.order("name").offset(@page*20).limit(20).all()
-  end
+  # def accounts
+  #  @page = (params[:page] || 1).to_i
+  #  @accounts = Account.order("name").offset(@page*20).limit(20).all()
+  #end
 
-  def account
-    if params[:id] =~ /^\d+$/
-        @account = Account.find(params[:id])
-    else
-        @account = Account.find_by_sfid(params[:id])
-    end
+  #def account
+  #  if params[:id] =~ /^\d+$/
+  #      @account = Account.find(params[:id])
+  #  else
+  #      @account = Account.find_by_sfid(params[:id])
+  #  end
 
-    begin
-      @contacts = @account.contacts
-    rescue
-      @contacts = []
-    end
-  end
+  #  begin
+  #    @contacts = @account.contacts
+  #  rescue
+  #    @contacts = []
+  #  end
+  #end
 
-  def contacts
-    @page = (params[:page] || 1).to_i
-    @contacts = Contact.order("lastname").offset(@page*20).limit(20).all()
-  end
+  #def contacts
+  #  @page = (params[:page] || 1).to_i
+  #  @contacts = Contact.order("lastname").offset(@page*20).limit(20).all()
+  #end
 
 end
